11c9967b3897891ffd4054262e9782212eaaeba1
Mona <munaaljaali9375@gmail.com>
2023-12-05 18:30:57 -0800

    [add]: two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/px_thon3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if not tuple_a:
+        x, x_ = 0, 0
+    elif len(tuple_a) == 1:
+        x, x_ = tuple_a[0], 0
+    else:
+        x, x_ = tuple_a[0], tuple_a[1]
+    if not tuple_b:
+        y, y_ = 0, 0
+    elif len(tuple_b) == 1:
+        y, y_ = tuple_b[0], 0
+    else:
+        y, y_ = tuple_b[0], tuple_b[1]
+    result = (x + y, x_ + y_)
+    return result
