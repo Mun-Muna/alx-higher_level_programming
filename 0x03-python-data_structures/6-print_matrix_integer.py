620167911deac7e43fd433866cca8bf47425b38a
Mona <munaaljaali9375@gmail.com>
2023-12-05 17:02:01 -0800

    [fix]: checker output error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print('')
     for row in matrix:
         column = len(row)
         for i in range(column):
