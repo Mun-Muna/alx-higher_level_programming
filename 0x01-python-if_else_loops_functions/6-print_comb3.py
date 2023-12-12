33400132dcd82fbde5cfb9ea5840904303edb037
Mona <munaaljaali9375@gmail.com>
2023-12-11 17:33:05 -0800

    [Edit]: code to become more concise

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(10):
-        if j > i and i != 8:
-            print('{:d}{:d}'.format(i, j), end=', ')
-        elif i == 8 and j == 9:
-            print('{:d}{:d}'.format(i, j))
+        if j > i:
+            print('{:d}{:d}'.format(i, j), end=', ' if i != 8 else '\n')
