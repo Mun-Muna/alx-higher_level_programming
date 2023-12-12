4030ac7d3d82f194ae3e41b39b5174f4d24eed8a
Mona <munaaljaali9375@gmail.com>
2023-12-11 17:09:31 -0800

    [Print]: all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(10):
+        if j > i and (i != 8 and j != 9):
+            print('{:d}{:d}'.format(i, j), end=', ')
+        elif i == 8 and j == 9:
+            print('{:d}{:d}'.format(i, j))
