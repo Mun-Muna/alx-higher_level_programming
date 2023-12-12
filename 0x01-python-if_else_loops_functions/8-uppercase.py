c8b4f17a615eb134396a1229b5427337621c1b91
Mona <munaaljaali9375@gmail.com>
2023-12-11 18:09:24 -0800

    [Edit]: 97 to 96

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
     print("{}".format(''.join(
-         (chr(ord(c) - 32)) if 97 < ord(c) < 123 else c for c in str)))
+         (chr(ord(c) - 32)) if 96 < ord(c) < 123 else c for c in str)))
