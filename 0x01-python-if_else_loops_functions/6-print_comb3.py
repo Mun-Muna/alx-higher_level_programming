cbf3f798c9f3b3e30039c64e6c03a567b2d745e9
Mona <munaaljaali9375@gmail.com>
2023-12-11 17:17:51 -0800

    [Fix]: j == 9 instead of i == 9

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(9):
     for j in range(10):
         if j > i and i != 8:
             print('{:d}{:d}'.format(i, j), end=', ')
-        elif i == 8 and i == 9:
+        elif i == 8 and j == 9:
             print('{:d}{:d}'.format(i, j))
