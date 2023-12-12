ae0cfc1c9a0a4b6d3052ec95e2670a25fe00185f
Mona <munaaljaali9375@gmail.com>
2023-12-11 17:14:39 -0800

    [Fix]: output error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(10):
-        if j > i and (i != 8 and j != 9):
+        if j > i and i != 8:
             print('{:d}{:d}'.format(i, j), end=', ')
-        elif i == 8 and j == 9:
+        elif i == 8 and i == 9:
             print('{:d}{:d}'.format(i, j))
