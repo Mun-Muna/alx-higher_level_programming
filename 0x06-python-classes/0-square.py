b480050174489d8d593d9d8033adcbc72a47276a
Mona <munaaljaali9375@gmail.com>
2023-12-19 18:12:27 -0800

    [fix]: documentation error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""This module defines an empty class Square."""
 class Square:
-    """This is an empty class defining the structure of a square"""
+    """
+    This is an empty class defining the structure of a square.
+
+    Attributes:
+        No attributes.
+    """
     pass
